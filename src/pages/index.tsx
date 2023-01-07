3e3dc4b1c5a36aaf5b64af44a9e745dc46a16824
Isaac Batista <isaacbatst@gmail.com>
2023-01-07 14:50:47 -0300

    update meta

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,9 +8,10 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Nova ficha de T20</title>
+        <title>Ficha de T20</title>
         <meta name="description" content="Construa sua ficha do zero" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="google-site-verification" content="A1PBUnkWuS_DQFxOtFEs984kX9ClcJV8JRASYzZY4Ao" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className='text-center'>
